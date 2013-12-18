174646579494a176503b110859f0520c969c62d7
auz34 <andrewzavgorodniy@gmail.com>
2013-12-18 08:28:18 +0200

    Further development for availability indicator

--- Controls/Orc.Toolkit.SL/AvailabilityIndicator/AvailabilityIndicator.cs
+++ Controls/Orc.Toolkit.SL/AvailabilityIndicator/AvailabilityIndicator.cs
@@ -9,6 +9,8 @@
 
 namespace Orc.Toolkit
 {
+    using System;
+    using System.Reactive.Linq;
     using System.Windows;
     using System.Windows.Controls;
 
@@ -17,16 +19,40 @@ namespace Orc.Toolkit
     /// </summary>
     [TemplateVisualState(Name = "NotAvailable", GroupName = "MainStates")]
     [TemplateVisualState(Name = "Available", GroupName = "MainStates")]
+    [TemplateVisualState(Name = "IsInUse", GroupName = "MainStates")]
     public class AvailabilityIndicator : Control
     {
         /// <summary>
         /// The availability property.
         /// </summary>
-        public static readonly DependencyProperty AvailabilityProperty = DependencyProperty.Register(
-            "Availability",
+        public static readonly DependencyProperty IsAvailableProperty = DependencyProperty.Register(
+            "IsAvailable",
             typeof(bool),
             typeof(AvailabilityIndicator),
-            new PropertyMetadata(new PropertyChangedCallback(OnAvailabilityChanged)));
+            new PropertyMetadata(false, new PropertyChangedCallback(OnStateChanged)));
+
+        /// <summary>
+        /// The is in use property.
+        /// </summary>
+        public static readonly DependencyProperty IsInUseProperty = DependencyProperty.Register(
+            "IsInUse",
+            typeof(bool),
+            typeof(AvailabilityIndicator),
+            new PropertyMetadata(false, new PropertyChangedCallback(OnIsInUseChanged)));
+
+        /// <summary>
+        /// The in use period property.
+        /// </summary>
+        public static readonly DependencyProperty InUsePeriodProperty = DependencyProperty.Register(
+            "InUsePeriod",
+            typeof(double),
+            typeof(AvailabilityIndicator),
+            new PropertyMetadata(0.0, new PropertyChangedCallback(OnInUsePeriodChanged)));
+
+        /// <summary>
+        /// The in use started subscription.
+        /// </summary>
+        private IDisposable inUseStartedSubscription;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AvailabilityIndicator"/> class.
@@ -37,18 +63,130 @@ namespace Orc.Toolkit
         }
 
         /// <summary>
-        /// Gets or sets a value indicating whether availability.
+        /// The is in use started.
         /// </summary>
-        public bool Availability
+        private event EventHandler IsInUseStarted;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether service is available.
+        /// </summary>
+        public bool IsAvailable
         {
             get
             {
-                return (bool)this.GetValue(AvailabilityProperty);
+                return (bool)this.GetValue(IsAvailableProperty);
             }
 
             set
             {
-                this.SetValue(AvailabilityProperty, value);
+                this.SetValue(IsAvailableProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether service is available.
+        /// </summary>
+        public bool IsInUse
+        {
+            get
+            {
+                return (bool)this.GetValue(IsInUseProperty);
+            }
+
+            set
+            {
+                this.SetValue(IsInUseProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the in use period.
+        /// </summary>
+        public double InUsePeriod
+        {
+            get
+            {
+                return (double)this.GetValue(InUsePeriodProperty);
+            }
+
+            set
+            {
+                this.SetValue(InUsePeriodProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// The on apply template.
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            this.SetVisualState();
+            base.OnApplyTemplate();
+        }
+
+        /// <summary>
+        /// The on is in use started.
+        /// </summary>
+        protected virtual void OnIsInUseStarted()
+        {
+            EventHandler handler = this.IsInUseStarted;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+
+        /// <summary>
+        /// The on in use period changed.
+        /// </summary>
+        /// <param name="d">
+        /// The d.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private static void OnInUsePeriodChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            AvailabilityIndicator availabilityIndicator = d as AvailabilityIndicator;
+            if (availabilityIndicator != null)
+            {
+                if (availabilityIndicator.inUseStartedSubscription != null)
+                {
+                    availabilityIndicator.inUseStartedSubscription.Dispose();
+                }
+
+                if (availabilityIndicator.InUsePeriod > 0)
+                {
+                    availabilityIndicator.inUseStartedSubscription =
+                        Observable.FromEventPattern<EventHandler, EventArgs>(
+                            h => availabilityIndicator.IsInUseStarted += h,
+                            h => availabilityIndicator.IsInUseStarted -= h)
+                                  .Throttle(TimeSpan.FromMilliseconds(500))
+                                  .Delay(TimeSpan.FromMilliseconds(availabilityIndicator.InUsePeriod))
+                                  .Subscribe(pattern => availabilityIndicator.IsInUse = false);
+                }
+            }
+        }
+
+        /// <summary>
+        /// The on is in use changed.
+        /// </summary>
+        /// <param name="d">
+        /// The d.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private static void OnIsInUseChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            AvailabilityIndicator availabilityIndicator = d as AvailabilityIndicator;
+            if (availabilityIndicator != null)
+            {
+                availabilityIndicator.SetVisualState();
+                if (availabilityIndicator.IsInUse)
+                {
+                    availabilityIndicator.OnIsInUseStarted();
+                }
             }
         }
 
@@ -61,14 +199,28 @@ namespace Orc.Toolkit
         /// <param name="e">
         /// The e.
         /// </param>
-        private static void OnAvailabilityChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private static void OnStateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             AvailabilityIndicator availabilityIndicator = d as AvailabilityIndicator;
-            bool newState = (bool)e.NewValue;
+            if (availabilityIndicator != null)
+            {
+                availabilityIndicator.SetVisualState();
+            }
+        }
 
-            VisualStateManager.GoToState(availabilityIndicator, newState ? "Available" : "Notavailable", false);
-            var groups = VisualStateManager.GetVisualStateGroups(availabilityIndicator);
-            System.Diagnostics.Debug.WriteLine(groups.Count);
+        /// <summary>
+        /// The set visual state.
+        /// </summary>
+        private void SetVisualState()
+        {
+            if (!this.IsAvailable)
+            {
+                VisualStateManager.GoToState(this, "NotAvailable", false);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, this.IsInUse ? "IsInUse" : "Available", false);
+            }
         }
     }
 }
