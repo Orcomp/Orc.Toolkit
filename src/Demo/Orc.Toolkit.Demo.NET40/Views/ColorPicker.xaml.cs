3da966472db1d01a10d65d350b2262cce31150b2
Geert van Horrik <geert@catenalogic.com>
2014-08-08 17:12:41 +0200

    Major solution reorganization

---
+++ src/Demo/Orc.Toolkit.Demo.NET40/Views/ColorPicker.xaml.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ColorPicker.xaml.cs" company="ORC">
+//   MS-PL
+// </copyright>
+// <summary>
+//   Interaction logic for ColorPicker.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Orc.Toolkit.Demo.Views
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for ColorPicker.xaml
+    /// </summary>
+    public partial class ColorPicker : UserControl
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ColorPicker"/> class.
+        /// </summary>
+        public ColorPicker()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Demo/Orc.Toolkit.Demo.NET40/Views/DropDownButton.xaml.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DropDownButton.xaml.cs" company="ORC">
+//   MS-PL
+// </copyright>
+// <summary>
+//   Interaction logic for DropDownButton.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Orc.Toolkit.Demo.Views
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for DropDownButton.xaml
+    /// </summary>
+    public partial class DropDownButton : UserControl
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DropDownButton"/> class.
+        /// </summary>
+        public DropDownButton()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
