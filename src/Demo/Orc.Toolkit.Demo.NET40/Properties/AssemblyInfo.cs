94970087fb54ab8aa69bea3e6a6432f9cbd5835d
Geert van Horrik <geert@catenalogic.com>
2014-08-08 17:53:27 +0200

    Solution assemblyinfo improvements

--- src/Demo/Orc.Toolkit.Demo.NET40/Properties/AssemblyInfo.cs
+++ src/Demo/Orc.Toolkit.Demo.NET40/Properties/AssemblyInfo.cs
@@ -1,45 +1,20 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="ORC">
-//   MS-PL
+// <copyright file="AssemblyInfo.cs" company="Orchestra development team">
+//   Copyright (c) 2008 - 2014 Orchestra development team. All rights reserved.
 // </copyright>
-// <summary>
-//   AssemblyInfo.cs
-// </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
+
+// All other assembly info is defined in SharedAssembly.cs
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Windows;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Orc.Toolkit.Demo.WPF")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ORC")]
-[assembly: AssemblyProduct("Orc.Toolkit.Demo.WPF")]
-[assembly: AssemblyCopyright("Copyright © ORC 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Orc.Toolkit demo")]
+[assembly: AssemblyProduct("Orc.Toolkit demo")]
+[assembly: AssemblyDescription("Orc.Toolkit demo")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
-
-//In order to begin building localizable applications, set 
-//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-//inside a <PropertyGroup>.  For example, if you are using US english
-//in your source files, set the <UICulture> to en-US.  Then uncomment
-//the NeutralResourceLanguage attribute below.  Update the "en-US" in
-//the line below to match the UICulture setting in the project file.
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, // where theme specific resource dictionaries are located
-    // (used if a resource is not found in the page, 
-    // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly // where the generic resource dictionary is located
-    // (used if a resource is not found in the page, 
-    // app, or any theme specific resource dictionaries)
-)]
--- src/Orc.Toolkit/Orc.Toolkit.NET40/Properties/AssemblyInfo.cs
+++ src/Orc.Toolkit/Orc.Toolkit.NET40/Properties/AssemblyInfo.cs
@@ -1,31 +1,20 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="ORC">
-//   MS-PL
+// <copyright file="AssemblyInfo.cs" company="Orchestra development team">
+//   Copyright (c) 2008 - 2014 Orchestra development team. All rights reserved.
 // </copyright>
-// <summary>
-//   AssemblyInfo.cs
-// </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
+
+// All other assembly info is defined in SharedAssembly.cs
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Orc.Toolkit")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ORC")]
 [assembly: AssemblyProduct("Orc.Toolkit")]
-[assembly: AssemblyCopyright("Copyright © ORC 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("Orc.Toolkit")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("71d43758-fd29-45ef-a614-0255fdcec10b")]
+[assembly: ComVisible(false)]
